616bf5ee5995ab12390c4d2786aa4fde2286f1dd
vihnpalm <vplm.vn@gmail.com>
2022-12-13 14:42:30 +0700

    git commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices/enums';
+import { PaymentModule } from './payment/payment.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(PaymentModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port:5001
+    }
+  })
+
+  await app.startAllMicroservices();
+  await app.listen(5001);
+}
+bootstrap();
